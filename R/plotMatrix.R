b909759718088c0b164a56054b9082ef6e10655d
Simone Vazzoler <svazzole@gmail.com>
2016-02-26 18:19:56 +0100

    Plot

--- R/plotMatrix.R
+++ R/plotMatrix.R
@@ -16,12 +16,12 @@ plotMatrix <- function(M){
   nr <- nrow(M)
   nc <- ncol(M)
   colfunc<-colorRampPalette(c("red","white","royalblue"))  
-  M <- Matrix(M, sparse = TRUE)
-  image(M)
-  #image(1:nr, 1:nc, t(M)[,nc:1], col=colfunc(39), xlab = "Column Number", ylab = "Row number", yaxt = "n", xaxt = "n")
-  #axis(1, at = 1:nc, labels = as.character(1:nc)) 
-  #axis(2, at = 1:nc, labels = as.character(nc:1), las = 2) 
-  #axis(2, at = 1:nc, labels = as.character(nc:1)) 
+  # M <- Matrix(M, sparse = TRUE)
+  # image(M)
+  image(1:nr, 1:nc, t(M)[,nc:1], col=colfunc(39), xlab = "Column Number", ylab = "Row number", yaxt = "n", xaxt = "n")
+  axis(1, at = 1:nc, labels = as.character(1:nc))
+  # axis(2, at = 1:nc, labels = as.character(nc:1), las = 2)
+  axis(2, at = 1:nc, labels = as.character(nc:1))
   
 }
 

888bbdee2753c1733eb7dfe075de1e54e42f1448
Simone Vazzoler <svazzole@gmail.com>
2016-06-27 21:30:35 +0200

    Add std deviation of irf and oirf

--- R/impulseResponse.R
+++ R/impulseResponse.R
@@ -46,6 +46,7 @@ impulseResponse <- function(dataVar, len = 20) {
   out <- list()
   out$irf <- irf
   out$oirf <- oirf
+  out$cholP <- P
   attr(out, "class") <- "irf"
   
   return(out)
@@ -104,12 +105,16 @@ checkImpulseZero <- function(irf) {
   return(which(logicalIrf == TRUE, arr.ind = TRUE))
 }
 
-errorBandsIRF <- function(v, irf) {
+errorBandsIRF <- function(v, irf, alpha = 0.05) {
   
   M <- 100       # number of repeats
   lambda <- v$lambda 
   p <- length(v$A)
-
+  nc <- ncol(v$A[[1]]) 
+  len <- dim(irf$irf)[3]
+  
+  irfs <- array(data = rep(0,len*nc^2*M), dim = c(nc,nc,len+1, M))
+  oirfs <- array(data = rep(0,len*nc^2*M), dim = c(nc,nc,len+1, M))
   
   for (k in 1:M) {
     # create Xs and Ys (temp variables)
@@ -133,14 +138,59 @@ errorBandsIRF <- function(v, irf) {
     
     Avector <- stats::coef(fit, s = lambda)
     A <- matrix(Avector[2:length(Avector)], nrow = nc, ncol = nc*p, byrow = TRUE)
-    M <- cbind(diag(x = 1, nrow = (nc*(p-1)), ncol = (nc*(p-1))))
+    M <- cbind(diag(x = 1, nrow = (nc*(p-1)), ncol = (nc*(p-1))), matrix(0, nrow = (nc*(p-1)), ncol = nc))
     bigA <- rbind(A,M)  
+
+    tmpRes <- getIRF(v, bigA, len, irf$cholP)
+    irfs[,,,k] <- tmpRes$irf
+    oirfs[,,,k] <- tmpRes$oirf
+  
   }
   
+  irfUB <- array(data = rep(0,len*nr^2), dim = c(nr,nr,len))
+  oirfUB <- array(data = rep(0,len*nr^2), dim = c(nr,nr,len))
   
+  irfLB <- array(data = rep(0,len*nr^2), dim = c(nr,nr,len))
+  oirfLB <- array(data = rep(0,len*nr^2), dim = c(nr,nr,len))
+
+  irfSD <- array(data = rep(0,len*nr^2), dim = c(nr,nr,len))
+  oirfSD <- array(data = rep(0,len*nr^2), dim = c(nr,nr,len))
   
+  a <- alpha/2
   
-  return(o)
+  for (i in 1:nc) {
+    for (j in 1:nc) {
+      for (k in 1:len) {
+        iUB <- sd(irfs[i,j,k,])
+        oiUB <- sd(oirfs[i,j,k,])
+        iUB <- quantile(irfs[i,j,k,], probs = (1-a))
+        oiUB <- quantile(oirfs[i,j,k,], probs = (1-a))
+        irfUB[i,j,k] <- iUB
+        oirfUB[i,j,k] <- oiUB
+        iLB <- quantile(irfs[i,j,k,], probs = a)
+        oiLB <- quantile(oirfs[i,j,k,], probs = a)
+        irfLB[i,j,k] <- iLB
+        oirfLB[i,j,k] <- oiLB
+        iSD <- sd(irfs[i,j,k,])
+        oiSD <- sd(oirfs[i,j,k,])
+        irfSD[i,j,k] <- iSD
+        oirfSD[i,j,k] <- oiSD
+        
+      }
+    }
+  }
+  
+  output <- list()
+  output$irfUB <- irfUB
+  output$oirfUB <- oirfUB
+  
+  output$irfLB <- irfLB
+  output$oirfLB <- oirfLB
+  
+  output$irfSD <- irfSD
+  output$oirfSD <- oirfSD
+  
+  return(output)
 }
 
 genVARfromRes <- function(v) {
@@ -169,4 +219,31 @@ genVARfromRes <- function(v) {
   
   return(zt)
   
+}
+
+getIRF <- function (v, bigA, len = 20, P) {
+  
+  nr <- nrow(v$A[[1]])
+  
+  irf <- array(data = rep(0,len*nr^2), dim = c(nr,nr,len+1))
+  oirf <- array(data = rep(0,len*nr^2), dim = c(nr,nr,len+1))
+  
+  Atmp <- diag(nrow = nrow(bigA), ncol = ncol(bigA))
+  
+  irf[ , , 1] <- Atmp[1:nr, 1:nr] 
+  oirf[ , , 1] <- Atmp[1:nr, 1:nr] %*% P
+  
+  for (k in 1:len) {
+    Atmp <- Atmp %*% bigA
+    irf[ , , (k+1)] <- as.matrix(Atmp[1:nr, 1:nr])
+    oirf[ , , (k+1)] <- as.matrix(Atmp[1:nr, 1:nr] %*% P)
+  }
+  
+  ## TODO: add cumulative response functions
+  out <- list()
+  out$irf <- irf
+  out$oirf <- oirf
+  attr(out, "class") <- "irf"
+  
+  return(out)
 }

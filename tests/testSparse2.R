26c6749bfbb98ee6733a1b74e85072d066e9f672
Simone Vazzoler <svazzole@gmail.com>
2016-10-12 11:32:16 +0200

    Found a problem with scad2 for nlambda > 20

--- tests/testSparse2.R
+++ tests/testSparse2.R
@@ -4,7 +4,7 @@ Rcpp::sourceCpp(file = "/home/svazzole/workspace/r/ncvreg/src/maxprod.cpp")
 Rcpp::sourceCpp(file = "/home/svazzole/workspace/r/ncvreg/src/standardize.cpp")
 source(file = "/home/svazzole/workspace/r/sparsevar/R/scadReg.R")
 n <- 200
-p <- 20
+p <- 23
 
 X <- matrix(rnorm(n*p), n, p)
 mult <- rep(1, ncol(X))
@@ -12,11 +12,11 @@ b <- rnorm(p)
 y <- rnorm(n, X%*%b)
 beta <- lm(y~X)$coef
 
-XX <- as(Matrix::Matrix(X, sparse = TRUE), "dgCMatrix")
-scad <- scadReg(X,y)
-scad2 <- scadReg(XX,y,lambda=c(0), eps=.0001)
-scad3 <- scadReg(X,y, nlambda = 10)
-ncv <- coef(ncvreg(X,y,lambda=0,penalty="SCAD",eps=.0001))
+# XX <- as(Matrix::Matrix(X, sparse = TRUE), "dgCMatrix")
+scad <- scadReg(X,y,lambda=c(0),eps=.0001)
+scad2 <- scadReg(X,y,lambda=c(0), eps=.0001)
+scad3 <- scadReg(X,y, nlambda = 20, lambda.min = 0, eps = .0001)
+ncv <- coef(ncvreg(X,y,nlambda = 20, penalty="SCAD",eps=.0001))
 beta
 scad
 scad2
@@ -25,3 +25,8 @@ ncv
 check(scad, beta[2:nrow(beta)], tolerance=.01, check.attributes=FALSE)
 check(mcp, beta,tolerance=.01,check.attributes=FALSE)
 
+scad3$beta[, 21] - beta[2:24]
+
+abs((scad3$beta[,6]-scad3$beta[,5])/scad3$beta[,5])>.0001
+
+abs((scad3_2$beta[,6]-scad3_2$beta[,5])/scad3_2$beta[,5])>.0001

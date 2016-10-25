8468badc11d28b2cfda31d4583bbf7c66a8201f5
Simone Vazzoler <svazzole@gmail.com>
2016-10-25 11:45:52 +0200

    Fixes AIC

--- R/utilsVAR.R
+++ R/utilsVAR.R
@@ -329,11 +329,15 @@ informCrit <- function(v) {
       sigma <- v[[i]]$sigma
       nr <- nrow(v[[i]]$residuals)
       nc <- ncol(v[[i]]$residuals)
-      S <- (nr / (nr - nc * p - 1)) * sigma
-      d <- det(S)
+      # S <- (nr / (nr - nc * p - 1)) * sigma
+      # d <- det(S)
+      d <- det(sigma)
       r[i,1] <- log(d) + (2*p*nc^2)/nr                 # AIC
       r[i,2] <- log(d) + (p*nc^2)/nr * log(nr)         # Schwarz
       r[i,3] <- log(d) + (2*p*nc^2)/nr * log(log(nr))  # Hannan-Quinn
+      # r[i,1] <- nr*nc*log(2*pi) + nr*log(det(sigma)) + (2*p*nc^2) + nc*(nc+1) # AIC
+      # r[i,2] <- log(d) + (p*nc^2)/nr * log(nr)         # Schwarz
+      # r[i,3] <- log(d) + (2*p*nc^2)/nr * log(log(nr))  # Hannan-Quinn
     }
     results <- data.frame(r)
     colnames(results) <- c("AIC", "Schwarz", "HannanQuinn")

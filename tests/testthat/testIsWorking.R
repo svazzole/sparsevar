46de1d723d6f019f677ba6818c7faa5fb6c69dd4
Simone Vazzoler <svazzole@gmail.com>
2019-06-28 21:49:17 +0200

    Fix list of lambdas in FitVAR

--- tests/testthat/testIsWorking.R
+++ tests/testthat/testIsWorking.R
@@ -15,6 +15,13 @@ test_that("sparsevar", {
   expect_output(str(fit), "List of 11")
   expect_output(cat(attr(fit, "class")), "var")
   expect_output(cat(attr(fit, "type")), "fit")
+  
+  sim <- sparsevar::simulateVAR(N = 30, p = 1)
+  fit <- sparsevar::fitVAR(sim$series, p = 1, 
+                           lambdas_list = c(0.1, 0.5, 0.3))
+  expect_output(str(fit), "List of 11")
+  expect_output(cat(attr(fit, "class")), "var")
+  expect_output(cat(attr(fit, "type")), "fit")
 })
 
 context("Testing IRF and error bands")
